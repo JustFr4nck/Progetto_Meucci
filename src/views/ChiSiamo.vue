df331343f54fe462bb730645bf12b777e2b40656
JustFr4nck <francyperry07@gmail.com>
2025-11-23 19:17:08 +0100

    chi siamo added

--- src/views/ChiSiamo.vue
+++ src/views/ChiSiamo.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="pt-80">
-    <p>Qua ci sarà scritto chi siamo</p>
+  <div class="flex flex-col-reverse md:flex-row items-center justify-center gap-10 px-6 md:px-20 pt-20 lg:pt-60 pb-20">
+
+    <div class="w-full md:w-1/2 md:text-left text-center">
+      <h1 class="text-3xl lg:text-4xl font-extrabold textSub roboto-slab">Chi siamo</h1>
+      <p class="textTit lg:text-xl text-lg leading-relaxed roboto-slab mt-4">
+        Siamo due studenti di quinta che tengono davvero alla nostra scuola.
+        Ci candidiamo perché vogliamo lasciare qualcosa di positivo prima di andarcene:
+        piccole cose concrete che possano migliorare la vita di tutti i giorni.
+        Vogliamo ascoltare le idee degli altri, capire cosa serve davvero e impegnarci
+        per farlo succedere nel modo più pratico possibile.
+      </p>
+    </div>
+
+    <div class="w-full md:w-1/2 flex justify-center md:justify-end">
+      <img
+        src="../assets/velaMare.jpeg"
+        alt="Foto"
+        class="rounded-2xl object-cover w-full max-w-md shadow-lg lg:mr-30"
+      >
+    </div>
+
   </div>
 </template>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <MyNavbar/>
   </div>
 
-  <div class="flex flex-col md:flex-row items-center justify-center backGroundMain p-6 md:p-10 gap-6 lg:mt-40">
+  <div class=" lg:pl-50 flex flex-col md:flex-row items-center justify-center backGroundMain p-6 md:p-10 gap-6 lg:mt-40">
 
 
     <div class="flex shrink-0">
@@ -15,14 +15,13 @@
     </div>
 
 
-    <div class="flex flex-col text-center md:text-left lg:ml-10">
-      <h1 class="text-4xl font-extrabold textTit roboto-slab">PROGETTO MEUCCI</h1>
+    <div class="flex flex-col text-center md:text-left lg:ml-10 lg:pr-80">
+      <h1 class="text-3xl lg:text-4xl font-extrabold textTit roboto-slab">PROGETTO MEUCCI</h1>
       <h2 class="text-2xl font-bold textSub roboto-slab">"La scuola che vogliamo, la costruiamo insieme"</h2>
-      <p class="text-xl  mt-2 textTit roboto-slab">
-        La nostra lista si impegna a migliorare la scuola attraverso progetti concreti e inclusivi,
-        <br>pensati per valorizzare ogni studente.
+      <p class="lg:text-xl  text-lg  mt-2 textTit roboto-slab">
+        La nostra lista si impegna a migliorare la scuola attraverso progetti concreti e inclusivi, pensati per valorizzare ogni studente.
         <br>Vogliamo rendere l’ambiente scolastico più accogliente,
-        <br>sicuro e stimolante, promuovendo attività che favoriscano collaborazione, partecipazione e benessere.
+        sicuro e stimolante, promuovendo attività che favoriscano collaborazione, partecipazione e benessere.
         <br>Il nostro obiettivo è costruire una comunità scolastica più moderna,
         <br>attenta alle esigenze di tutti e capace di offrire nuove opportunità di crescita.
       </p>
@@ -40,16 +39,3 @@ import MyNavbar from '@/components/MyNavbar.vue';
 import ChiSiamo from './ChiSiamo.vue';
 </script>
 
-
-<style scoped>
-
-.textTit{
-
-  color: #042C54;
-}
-
-.textSub{
-  color: #FFC787
-}
-
-</style>
