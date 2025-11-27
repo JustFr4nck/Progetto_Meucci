b071c51b88246dcccf92efb2b3542314593cb7b7
JustFr4nck <francyperry07@gmail.com>
2025-11-27 19:42:24 +0100

    projects updated final

--- src/views/OurProjects.vue
+++ src/views/OurProjects.vue
@@ -10,15 +10,18 @@
       <div class="carousel rounded-box w-80">
         <div class=" flex flex-col items-center carousel-item w-full bg-caru">
           <img src="../assets/connected.png" class=" rounded-xl w-45 h-45 mt-5" />
-          <h1 class="textClear">...</h1>
+          <h1 class="textSub text-2xl font-bold mt-2 text-center">Sconti e servizi con <a class="font-bold" href="https://www.instagram.com/connected_firenze?igsh=MWZlaDZtYnBvejlocQ==">@connected_firenze</a>:</h1>
+          <h1 class="textClear text-md text-center ml-6 mr-6 mb-4">Una piattaforma ricca di funzionalità utili per gli studenti, che include anche sconti fino al 15% in numerose attività convenzionate.</h1>
         </div>
         <div class="flex flex-col items-center carousel-item w-full bg-caru">
           <img src="../assets/giovaniWannabe.jpeg" class="rounded-xl w-45 h-45 mt-5" />
           <h1 class="textSub text-2xl font-bold mt-2 text-center">Assemblee con <br> "Giovani Wannabe"</h1>
-          <h1 class="textClear text-md text-center ml-4 mr-4 mb-4">Nonostante le difficoltà, noi ci stiamo battendo per riportare le assemblee! Grazie all'aiuto di <a href="https://www.instagram.com/giovani.wannabe_?igsh=MTRkdzlmd3Boc24xOQ==">@giovani.wannabe_</a>, vogliamo dare voce agli studenti e renderli protagonisti della vita scolastica.</h1>
+          <h1 class="textClear text-md text-center ml-6 mr-6 mb-4">Nonostante le difficoltà, noi ci stiamo battendo per riportare le assemblee! Grazie all'aiuto di <a class="font-bold" href="https://www.instagram.com/giovani.wannabe_?igsh=MTRkdzlmd3Boc24xOQ==">@giovani.wannabe_</a>, vogliamo dare voce agli studenti e renderli protagonisti della vita scolastica.</h1>
         </div>
-        <div class="carousel-item w-full">
-          <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" class="w-full" />
+        <div class="flex flex-col items-center carousel-item w-full bg-caru">
+          <img src="../assets/cpsFi.jpg" class="rounded-xl w-45 h-45 mt-5" />
+          <h1 class="textSub text-2xl font-bold mt-2 text-center">Il potere della Consulta</h1>
+          <h1 class="textClear text-md text-center ml-6 mr-6 mb-4">Come membri della <a class="font-bold" href="https://www.instagram.com/cps_firenze?igsh=bnUzejUzempmcHhx">Consulta degli Studenti di Firenze</a>, siamo pronti ad accogliere le vostre idee e a portarle direttamente al tavolo del confronto, trasformandole in proposte concrete per una scuola migliore.</h1>
         </div>
       </div>
     </div>
